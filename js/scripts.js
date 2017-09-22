0d31f30b2266e12b477fc9559a548ffe284aa15e
Victor Puentes <vmpuentes@gmail.com>
2017-09-22 16:20:41 -0700

    add changes to the submit function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,29 @@
 //Business Logic
 
 var pingPongArray = ["Ping", "Pong", "PING-PONG"];
+var outputNumber =
+var outputArray = [];
+for (var i = 0, i<= (pingPong.length), i ++ )
+  if (i % 3 === 0) {
+    $(outputArray).append('<img src="img/gump.png">PING-PONG!').hide().fadein(200);
+  else if (i % 5 === 0) {
+    $(outputArray).append();
+  } else if (i % 15 === 0) {
+    $(outputArray).append();
+  }
+}
+}
+
+event.preventDefault();
+return outputArray;
+}
 
 
 //User Logic
 $(document).ready(function() {
-  $(#formOne).submit(function() {
-    var userInput = $("#numberInput").val().split(" ").join("");
-    var outputNumber =
-    var outputArray = [];
-  for (var i = 0, i<= (pingPong.length), i ++ )
+  $("#formOne").submit(function(event) {
+    var userInput = parseInt($("#numberInput").val());
+    console.log(userInput);
+
   }
 }
-
-event.preventDefault();
