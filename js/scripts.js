80a76a3723193251f2404653ba0697f6b05b662b
Victor Puentes <vmpuentes@gmail.com>
2017-09-22 16:49:40 -0700

    add splice method and appendmethod to show output result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 //Business Logic
 
 var pingPongArray = ["Ping", "Pong", "PING-PONG"];
-var outputNumber =
-var outputArray = [];
+//var outputArray = [(<li>" "</li>)];
 for (var i = 0, i<= (pingPong.length), i ++ )
   if (i % 3 === 0) {
-    $(outputArray).append('<img src="img/gump.png">PING-PONG!').hide().fadein(200);
-  else if (i % 5 === 0) {
-    $(outputArray).append();
+    var outputNumber = pingPongArray.splice(pos, 0).append('<img src="img/gump.png">PING-PONG!').hide().fadein(200);
+  ] else if (i % 5 === 0) {
+    var outPutNumber = pingPongArray.splice(pos, 1).append('PONG');
   } else if (i % 15 === 0) {
-    $(outputArray).append();
+    var outPutNumber = pingPongArray.splice(pos, 2).append('PING-PONG').hide().fadein();
   }
 }
 }
