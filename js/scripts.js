8a5400dbfb8b1d63f53cb861033b858b4035e070
Victor Puentes <vmpuentes@gmail.com>
2017-09-22 15:17:16 -0700

    add variables and, global and local to function in scripts file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 //Business Logic
 
+var pingPong = ["Ping", "Pong", "PING-PONG"];
 
 
 //User Logic
 $(document).ready(function() {
-
+  $(#formOne).submit(function() {
+    var userInput = $("#numberInput").val().split(" ").join("");
+    var outputNumber =
+    var newArray = [];
+  for (var index )
+  }
 }
+
+event.preventDefault();
