e689c194b3517a69bec189d3ed9b1c74e8fd6731
Victor Puentes <vmpuentes@gmail.com>
2017-09-25 20:28:24 -0700

    take away global array in scripts file, use a global function instead.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 //Business Logic
 
-var pingPongArray = ["Ping", "Pong", "PING-PONG"];
+//---My first idea was to make an array of the ping pong phrases and then call upon them
+//---with a call back function in the for loop, and using the .append method to display them in
+//---in the HTML file.
+//var pingPongArray = ["Ping", "Pong", "PING-PONG"];
 //var outputArray = [(<li>" "</li>)];
 for (var i = 0, i<= (pingPong.length), i ++ )
   if (i % 3 === 0) {
     var outputNumber = pingPongArray.splice(pos, 0).append('<img src="img/gump.png">PING-PONG!').hide().fadein(200);
   ] else if (i % 5 === 0) {
-    var outPutNumber = pingPongArray.splice(pos, 1).append('PONG');
+    var outPutNumber = pingPongArray.splice(pos, 1).append('<img src="img/PONG').hide().fadein(200);
   } else if (i % 15 === 0) {
-    var outPutNumber = pingPongArray.splice(pos, 2).append('PING-PONG').hide().fadein();
+    var outPutNumber = pingPongArray.splice(pos, 2).append('PING-PONG').hide().fadein(200);
   }
-}
-}
-
-event.preventDefault();
 return outputArray;
 }
 
@@ -22,6 +21,7 @@ return outputArray;
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var userInput = parseInt($("#numberInput").val());
+    event.preventDefault();
     console.log(userInput);
 
   }
